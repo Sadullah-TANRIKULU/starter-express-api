0251dadfa684ddf1a596e8fc43e37d49b9a757f1
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-18 17:36:39 +0300

    aws s3 installed

--- index.js
+++ index.js
@@ -1,26 +1,78 @@
 const express = require('express')
 const app = express()
-
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3()
+const bodyParser = require('body-parser');
 
-// store something
-await s3.putObject({
-                Body: JSON.stringify({key:"value"}),
-                Bucket: "cyclic-splendid-rose-snapper-eu-north-1",
-                Key: "some_files/my_file.json",
-            }).promise()
+app.use(bodyParser.json())
 
-// get it back
-let my_file = await s3.getObject({
-                Bucket: "cyclic-splendid-rose-snapper-eu-north-1",
-                Key: "some_files/my_file.json",
-            }).promise()
+// curl -i https://some-app.cyclic.app/myFile.txt
+app.get('*', async (req,res) => {
+  let filename = req.path.slice(1)
 
-console.log(JSON.parse(my_file))
+  try {
+    let s3File = await s3.getObject({
+      Bucket: process.env.BUCKET,
+      Key: filename,
+    }).promise()
+
+    res.set('Content-type', s3File.ContentType)
+    res.send(s3File.Body.toString()).end()
+  } catch (error) {
+    if (error.code === 'NoSuchKey') {
+      console.log(`No such key ${filename}`)
+      res.sendStatus(404).end()
+    } else {
+      console.log(error)
+      res.sendStatus(500).end()
+    }
+  }
+})
+
+
+// curl -i -XPUT --data '{"k1":"value 1", "k2": "value 2"}' -H 'Content-type: application/json' https://some-app.cyclic.app/myFile.txt
+app.put('*', async (req,res) => {
+  let filename = req.path.slice(1)
+
+  console.log(typeof req.body)
+
+  await s3.putObject({
+    Body: JSON.stringify(req.body),
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'text/plain')
+  res.send('ok').end()
+})
+
+// curl -i -XDELETE https://some-app.cyclic.app/myFile.txt
+app.delete('*', async (req,res) => {
+  let filename = req.path.slice(1)
+
+  await s3.deleteObject({
+    Bucket: process.env.BUCKET,
+    Key: filename,
+  }).promise()
+
+  res.set('Content-type', 'text/plain')
+  res.send('ok').end()
+})
+
+// /////////////////////////////////////////////////////////////////////////////
+// Catch all handler for all other request.
+app.use('*', (req,res) => {
+  res.sendStatus(404).end()
+})
+
+// /////////////////////////////////////////////////////////////////////////////
+// Start the server
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+  console.log(`index.js listening at http://localhost:${port}`)
+})
 
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
-app.listen(process.env.PORT || 3000)
