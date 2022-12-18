0bf25dbd2e32e31bcac5b772006cf87c29b941fe
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-12-18 17:24:59 +0300

    first deploy expressjs on cyclic

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 const express = require('express')
 const app = express()
+
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
+
+// store something
+await s3.putObject({
+                Body: JSON.stringify({key:"value"}),
+                Bucket: "cyclic-splendid-rose-snapper-eu-north-1",
+                Key: "some_files/my_file.json",
+            }).promise()
+
+// get it back
+let my_file = await s3.getObject({
+                Bucket: "cyclic-splendid-rose-snapper-eu-north-1",
+                Key: "some_files/my_file.json",
+            }).promise()
+
+console.log(JSON.parse(my_file))
+
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
